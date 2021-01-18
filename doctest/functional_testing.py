c72e4fdb1a710c566f7cd1a9c3816a9238590c39
Sri Manikanta Palakollu <srimani.scholar@gmail.com>
2021-01-18 12:17:28 +0530

    Functional Testing with Doctest.

---
+++ doctest/functional_testing.py
@@ -0,0 +1,12 @@
+def add_two_numbers(a, b):
+    '''
+        Performing the doctest for this function
+
+        >>> add_two_numbers(7, 7)
+        14
+        >>> add_two_numbers(7, 4)
+        11
+        >>> add_two_numbers(7, -4)
+        3
+    '''
+    return a + b
